eec8c86b43fe65ba28a0c1e7ed68625d252085a4
Abakar <abakargargoum9@gmail.com>
2025-12-31 16:47:43 +0100

    Add test @smoke Login fails with invalid credentials

--- tests/orangeHRM/auth-login.spec.ts
+++ tests/orangeHRM/auth-login.spec.ts
@@ -1,8 +1,21 @@
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import { OrangeHRM } from "../pages/orangeHRM";
 
-test("Admin login", async ({ page }) => {
+test("@smoke Login success with valid credentials", async ({ page }) => {
   const orangeHRM = new OrangeHRM(page);
   await orangeHRM.Home();
   await orangeHRM.logIn("admin", "Abandass-2024");
+  await expect(
+    page.getByRole("heading", { name: "Dashboard" })
+  ).toBeVisible();
+  
+});
+test("@smoke Login fails with invalid credentials", async ({ page }) => {
+  const orangeHRM = new OrangeHRM(page);
+  await orangeHRM.Home();
+  await orangeHRM.logIn("adm", "Abandas");
+ await expect(
+    page.getByText("Invalid credentials")
+  ).toBeVisible();
+  
 });

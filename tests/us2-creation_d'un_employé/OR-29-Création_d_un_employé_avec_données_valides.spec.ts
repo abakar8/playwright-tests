f94596995a6ea34ed03991f438c60ea5e862c793
Abakar <abakargargoum9@gmail.com>
2026-02-11 08:24:00 +0100

    renommages de ct us2

---
+++ tests/us2-creation_d'un_employé/OR-29-Création_d_un_employé_avec_données_valides.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '../../fixtures/test-fixtures';
+import { getEnvironment } from '../../config/environments';
+import { createEmployee } from '../../Supports/employee-types';
+test.describe('Ajout d\'employé', () => {
+  test.beforeEach(async ({ loginPage, dashboardPage }) => {
+    await loginPage.navigate();
+    const env = getEnvironment();
+    await loginPage.login(env.username!, env.password!);
+    await dashboardPage.navigateToPIM();
+  });
+
+  test('OR-30_US-2: Ajouter un nouvel employé avec succès', async ({ addEmployeePage }) => {
+    test.setTimeout(60000);
+    const employee = createEmployee({
+      firstName: 'Jean',
+      lastName: 'Dupont',
+      employeeId: '123425',
+      nationality: 'Albanian',
+      matrialstaut: 'Single',
+      //dateOfBirth: '1990-01-01',
+      sexe: 'Male',
+      
+      
+    });
+
+    await addEmployeePage.clickAddEmployee(employee);
+    await addEmployeePage.saveEmployee();
+    //await addEmployeePage.fillEmployeeDetails(employee);
+    //await addEmployeePage.saveEmployee();
+     
+
+    // Vérification du succès - peut varier selon la version d'OrangeHRM
+    //await expect(addEmployeePage.page).toHaveURL(/.*employee/);
+  });   
+});
